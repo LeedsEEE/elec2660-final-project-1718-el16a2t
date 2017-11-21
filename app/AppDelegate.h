f5dcda3c8a7d9849a8215de80f8d9593544bbf78
Adam Thompson [el16a2t] <el16a2t@eentpc252.leeds.ac.uk>
2017-11-21 12:37:34 +0000

    Initial Commit

---
+++ app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  app
+//
+//  Created by Adam Thompson [el16a2t] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  app
+//
+//  Created by Adam Thompson [el16a2t] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *topimage;
+
+@end
+
