bf15ee9652a27f161a68b0e3d12913653e686337
Adam Thompson [el16a2t] <el16a2t@eentpc95.leeds.ac.uk>
2017-12-01 19:30:12 +0000

    Initial Commit

---
+++ app/BottomViewController.h
@@ -0,0 +1,15 @@
+//
+//  BottomViewController.h
+//  app
+//
+//  Created by Adam Thompson [el16a2t] on 01/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BottomViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *Shoes;
+
+@end
